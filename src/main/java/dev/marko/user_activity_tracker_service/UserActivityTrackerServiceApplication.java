ccc3ce1a5808dba466ee138e5a7c08fcc1d1f9ca
Marko Cevrljakovic <marecevrljakovic@gmail.com>
2025-10-05 19:48:54 +0200

    Initial commit

---
+++ src/main/java/dev/marko/user_activity_tracker_service/UserActivityTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.marko.user_activity_tracker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserActivityTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserActivityTrackerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marko/user_activity_tracker_service/UserActivityTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marko.user_activity_tracker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActivityTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
